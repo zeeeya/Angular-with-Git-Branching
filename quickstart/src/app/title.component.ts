e42479dc7d4a4962970b994c05eab0d86a763df7
zeeeya <zci.alvarez@gmail.com>
2018-01-08 22:25:12 +0800

    modified title.component.html & title.component.ts

--- quickstart/src/app/title.component.ts
+++ quickstart/src/app/title.component.ts
@@ -1,15 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-title',
-  templateUrl: './title.component.html',
-  styles: []
+  templateUrl: './title.component.html'
 })
-export class TitleComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class TitleComponent {
+  title = 'Angular Modules';
 }
