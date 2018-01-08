ec28ff7fa04d6e5b02070ff0c703b03fec5611dc
zeeeya <zci.alvarez@gmail.com>
2018-01-08 21:44:23 +0800

    Modified app.component.ts

--- quickstart/src/app/app.component.ts
+++ quickstart/src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: '<h1>{{title}}</h1>',
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Angular Modules';
 }
